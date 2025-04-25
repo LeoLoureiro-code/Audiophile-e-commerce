fb7eb1e9294d54768ed03126267b9ab79bd1d91c
Leo Loureiro <leolou.90@gmail.com>
2025-04-24 19:02:42 -0700

    add category-options component

---
+++ src/components/category-options/category-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'category-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-options.component.html',
+  styleUrl: './category-options.component.scss'
+})
+export class CategoryOptionsComponent {
+
+}
