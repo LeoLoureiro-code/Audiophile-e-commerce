dba7699c1f4ef1457f1f1bcddc41e9ba22b0eb52
Leo Loureiro <leolou.90@gmail.com>
2025-04-28 18:59:39 -0700

    add routes to home, category and details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { CategoryComponent } from '../components/category/category.component';
+import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'category', component: CategoryComponent },
+    { path: 'details', component: ProductDetailComponent },
+];
