758c276a27b6ac76fc330d9c8b214068cfc40594
Leo Loureiro <leolou.90@gmail.com>
2025-04-30 19:03:20 -0700

    add new tag, image locations and styles to secondary and tertiary products

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CategoryOptionsComponent } from "../category-options/category-options.component";
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [CategoryOptionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
