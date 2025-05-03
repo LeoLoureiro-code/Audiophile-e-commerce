133e8731ac5d8fab0e24d548c31af59972d45ead
Leo Loureiro <leolou.90@gmail.com>
2025-05-02 20:49:37 -0700

    add category component

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CategoryOptionsComponent } from "../category-options/category-options.component";
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CategoryOptionsComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
